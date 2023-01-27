f07bcd31c8b0401bb0e9a7c4c40dc6d90311972b
OTakumi <takumi.o.0410@gmail.com>
2023-01-27 11:00:26 +0900

    add lib

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -74,6 +74,9 @@ return require('packer').startup(function(use)
 	-- Terminal
 	use 'voldikss/vim-floaterm'
 
+	-- Omnisharp
+	use 'OmniSharp/omnisharp-vim'
+
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then
