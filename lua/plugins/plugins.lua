31c4c9f1df779ee6fe3d5019ea907d5ed5a251f3
OTakumi <takumi.o.0410@gmail.com>
2023-01-28 22:02:04 +0900

    add new plugin

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -8,78 +8,91 @@ vim.cmd([[
 ]])
 
 return require('packer').startup(function(use)
-	-- Packer can manage itself
-	use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-	-- Simple plugins can be specified as strings
-	use 'rstacruz/vim-closer'
+    -- Simple plugins can be specified as strings
+    use 'rstacruz/vim-closer'
 
-	-- nvim-tree
-	use {
-		'nvim-tree/nvim-tree.lua',
-		require = {
-			'nvim-tree/nvim-web-devicons', -- optional, for file icons
-		},
-		tag = 'nightly' --optional, updated every week.
-	}
+    -- nvim-tree
+    use {
+        'nvim-tree/nvim-tree.lua',
+        require = {
+            'nvim-tree/nvim-web-devicons', -- optional, for file icons
+        },
+        tag = 'nightly' --optional, updated every week.
+    }
 
-	-- file finder
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.1',
-		requires = { {'nvim-lua/plenary.nvim'} }
-	}
+    -- file finder
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.1',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
 
-	-- LSP
-	use 'williamboman/mason.nvim'
-	use 'williamboman/mason-lspconfig.nvim'
-	use 'neovim/nvim-lspconfig' -- enable LSP
-	-- use 'williamboman/nvim-lsp-installer'	-- simple to use language server installer
-	use 'jose-elias-alvarez/null-ls.nvim' -- for formatters and linters
-	use 'glepnir/lspsaga.nvim' -- LSP UIs
-	use 'simrat39/rust-tools.nvim'
+    -- LSP
+    use 'williamboman/mason.nvim'
+    use 'williamboman/mason-lspconfig.nvim'
+    use 'neovim/nvim-lspconfig' -- enable LSP
+    -- use 'williamboman/nvim-lsp-installer'    -- simple to use language server installer
+    use 'jose-elias-alvarez/null-ls.nvim' -- for formatters and linters
+    use 'glepnir/lspsaga.nvim' -- LSP UIs
+    use 'simrat39/rust-tools.nvim'
 
-	-- cmp plugins
-	use 'hrsh7th/nvim-cmp' -- The completion plugins
-	use 'hrsh7th/cmp-buffer' -- buffer completions
-	use 'hrsh7th/cmp-path' -- path completions
-	use 'hrsh7th/cmp-cmdline' -- cmdline completions
-	use 'saadparwaiz1/cmp_luasnip' -- snippet completions
-	use 'hrsh7th/cmp-nvim-lsp'
-	use 'hrsh7th/cmp-nvim-lua'
-	use 'hrsh7th/cmp-vsnip'
-	use 'onsails/lspkind-nvim'
-	use 'hrsh7th/vim-vsnip'
+    -- cmp plugins
+    use 'hrsh7th/nvim-cmp' -- The completion plugins
+    use 'hrsh7th/cmp-buffer' -- buffer completions
+    use 'hrsh7th/cmp-path' -- path completions
+    use 'hrsh7th/cmp-cmdline' -- cmdline completions
+    use 'saadparwaiz1/cmp_luasnip' -- snippet completions
+    use 'hrsh7th/cmp-nvim-lsp'
+    use 'hrsh7th/cmp-nvim-lua'
+    use 'hrsh7th/cmp-vsnip'
+    use 'onsails/lspkind-nvim'
+    use 'hrsh7th/vim-vsnip'
 
-	-- snippets
-	use 'L3MON4D3/LuaSnip' -- snippet engine
+    -- snippets
+    use 'L3MON4D3/LuaSnip' -- snippet engine
 
-	-- DAP
-	use {
-		"rcarriga/nvim-dap-ui",
-		requires = {
-			'mfussenegger/nvim-dap',
-			'jayp0521/mason-nvim-dap.nvim',
-		},
-		config = function()
-			vim.fn.sign_define('DapBreakpoint', {text='■', texthl='', linehl='', numhl=''})
-			vim.fn.sign_define('DapStopped', {text='●', texthl='', linehl='', numhl=''})
-			require('dapui').setup()
-			require('dap.ext.vscode').load_launchjs()
-		end
-	}
+    -- DAP
+    use {
+        "rcarriga/nvim-dap-ui",
+        requires = {
+            'mfussenegger/nvim-dap',
+            'jayp0521/mason-nvim-dap.nvim',
+        },
+        config = function()
+            vim.fn.sign_define('DapBreakpoint', {text='■', texthl='', linehl='', numhl=''})
+            vim.fn.sign_define('DapStopped', {text='●', texthl='', linehl='', numhl=''})
+            require('dapui').setup()
+            require('dap.ext.vscode').load_launchjs()
+        end
+    }
 
-	-- Formatter & Linter
-	use 'MunifTanjim/prettier.nvim'
+    -- Formatter & Linter
+    use 'MunifTanjim/prettier.nvim'
 
-	-- Terminal
-	use 'voldikss/vim-floaterm'
+    -- Terminal
+    use 'voldikss/vim-floaterm'
 
-	-- Omnisharp
-	use 'OmniSharp/omnisharp-vim'
+    -- Omnisharp
+    use 'OmniSharp/omnisharp-vim'
 
-	-- Automatically set up your configuration after cloning packer.nvim
-	-- Put this at the end after all plugins
-	if PACKER_BOOTSTRAP then
-		require("packer").sync()
-	end
+    -- Todo highlight
+    use {
+        "folke/todo-comments.nvim",
+        requires = "nvim-lua/plenary.nvim",
+        -- config = function()
+            -- require("todo-comments").setup {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+            -- }
+        -- end
+    }
+
+    -- Automatically set up your configuration after cloning packer.nvim
+    -- Put this at the end after all plugins
+    if PACKER_BOOTSTRAP then
+        require("packer").sync()
+    end
 end)
