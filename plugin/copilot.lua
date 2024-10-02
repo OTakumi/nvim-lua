4335e77e86ceae083324f47504d00b32e6b3fec9
OTakumi <takumi.o.0410@gmail.com>
2024-10-02 13:56:45 +0900

    new file:   plugin/copilot.lua

---
+++ plugin/copilot.lua
@@ -0,0 +1,22 @@
+require("copilot").setup({
+    filetypes = {
+        javascript = true,
+        typescript = true,
+        typescriptreact = true,
+        javascriptreact = true,
+        lua = true,
+        python = true,
+        rust = true,
+        go = true,
+        sh = true,
+        yaml = true,
+        json = true,
+        html = true,
+        css = true,
+        help = true,
+        gitcommit = true,
+        gitrebase = true,
+        hgcommit = true,
+        ["*"] = false,
+    },
+})
