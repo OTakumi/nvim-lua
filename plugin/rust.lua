631939b00f86c802ae04e9f51480463b0f0710a6
OTakumi <takumi.o.0410@gmail.com>
2025-03-07 20:57:33 +0900

    feat(rust): enhance rust plugin configuration
    - Refactored rust plugin configuration to use a function.
    - Added key mappings for code actions and hover actions.
    - Enabled nextest and clippy tools.
    - Updated rust-analyzer settings for better linting and macro support.

--- plugin/rust.lua
+++ plugin/rust.lua
@@ -1,51 +1,42 @@
-local bufnr = vim.api.nvim_get_current_buf()
+vim.g.rustaceanvim = function()
+    local bufnr = vim.api.nvim_get_current_buf()
+    vim.keymap.set("n", "<leader>a", function()
+        vim.cmd.RustLsp("codeAction") -- supports rust-analyzer's grouping
+        -- or vim.lsp.buf.codeAction() if you don't want grouping.
+    end, { silent = true, buffer = bufnr })
+    vim.keymap.set(
+        "n",
+        "K", -- Override Neovim's built-in hover keymap with rustaceanvim's hover actions
+        function()
+            vim.cmd.RustLsp({ "hover", "actions" })
+        end,
+        { silent = true, buffer = bufnr }
+    )
 
-vim.keymap.set("n", "<leader>a", function()
-    vim.cmd.RustLsp("codeAction") -- supports rust-analyzer's grouping
-    -- or vim.lsp.buf.codeAction() if you don't want grouping.
-end, { silent = true, buffer = bufnr })
-
-vim.keymap.set(
-    "n",
-    "K", -- Override Neovim's built-in hover keymap with rustaceanvim's hover actions
-    function()
-        vim.cmd.RustLsp({ "hover", "actions" })
-    end,
-    { silent = true, buffer = bufnr }
-)
-
--- vim.g.rustaceanvim = function()
---     return {
---         -- other rustacean settings. --
---         server = {
---             on_attach = function()
---                 vim.keymap.set("n", "<Leader>K", function()
---                     vim.cmd.RustLsp({ "hover", "actions" })
---                 end, { buffer = bufnr })
---                 -- other settings. --
---             end,
---             default_settings = {
---                 -- rust-analyzer language server configuration
---                 ["rust-analyzer"] = {
---                     cargo = {
---                         allFeatures = true,
---                         loadOutDirsFromCheck = true,
---                         buildScripts = {
---                             enable = true,
---                         },
---                     },
---                     -- Add clippy lints for Rust.
---                     checkOnSave = true,
---                     procMacro = {
---                         enable = true,
---                         ignored = {
---                             ["async-trait"] = { "async_trait" },
---                             ["napi-derive"] = { "napi" },
---                             ["async-recursion"] = { "async_recursion" },
---                         },
---                     },
---                 },
---             },
---         },
---     }
--- end
+    return {
+        -- other rustacean settings. --
+        tools = {
+            enable_nextest = true,
+            enable_clippy = true,
+        },
+        server = {
+            on_attach = function()
+                -- other settings. --
+            end,
+            default_settings = {
+                -- rust-analyzer language server configuration
+                ["rust-analyzer"] = {
+                    -- Add clippy lints for Rust.
+                    checkOnSave = true,
+                    procMacro = {
+                        enable = true,
+                        ignored = {
+                            ["napi-derive"] = { "napi" },
+                            ["async-recursion"] = { "async_recursion" },
+                        },
+                    },
+                },
+            },
+        },
+    }
+end
