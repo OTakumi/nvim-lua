8fbe546b511a28a392756e56e9a1698477759e16
OTakumi <takumi.o.0410@gmail.com>
2025-07-04 21:12:36 +0900

    update

--- lua/lazy/lazy.lua
+++ lua/lazy/lazy.lua
@@ -165,5 +165,26 @@ require("lazy").setup({
     { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
 
     -- git diff
-    { "sindrets/diffview.nvim", lazy = true },
+    { "sindrets/diffview.nvim", lazy = true, event = "VimEnter" },
+
+    {
+        "kdheepak/lazygit.nvim",
+        lazy = true,
+        cmd = {
+            "LazyGit",
+            "LazyGitConfig",
+            "LazyGitCurrentFile",
+            "LazyGitFilter",
+            "LazyGitFilterCurrentFile",
+        },
+        -- optional for floating window border decoration
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        -- setting the keybinding for LazyGit with 'keys' is recommended in
+        -- order to load the plugin when the command is run for the first time
+        keys = {
+            { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+        },
+    },
 })
