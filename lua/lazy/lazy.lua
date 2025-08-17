1c27cc737d3d49fadfc249f1c09e0f6f4d8cad31
OTakumi <takumi.o.0410@gmail.com>
2025-08-17 17:21:02 +0900

    feat: conform.nvimとLSP設定の整理と拡張
    - conform.nvimの設定を整理し、Go言語のフォーマッタを追加
    - stylua/blackの引数や設定方法を修正
    - mason-lspconfigのensure_installedを各言語ごとに明示化
    - LSPのlua, python, csharp, html, cssの設定を整理・拡張
    - conform.nvimにmason依存を追加しlazyロードに変更

--- lua/lazy/lazy.lua
+++ lua/lazy/lazy.lua
@@ -1,197 +1,199 @@
 require("lazy").setup({
-    -- Github copilot
-    -- { "github/copilot.vim", lazy = false },
-    {
-        "zbirenbaum/copilot.lua",
-        cmd = "Copilot",
-        lazy = false,
-        opts = {
-            panel = { enabled = false },
-            suggestion = { enabled = false },
-        },
+  -- Github copilot
+  -- { "github/copilot.vim", lazy = false },
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    lazy = false,
+    opts = {
+      panel = { enabled = false },
+      suggestion = { enabled = false },
     },
+  },
 
-    {
-        "CopilotC-Nvim/CopilotChat.nvim",
-        dependencies = {
-            { "zbirenbaum/copilot.lua" }, -- or zbirenbaum/copilot.lua
-            { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
-        },
-        build = "make tiktoken", -- Only on MacOS or Linux
-        opts = {
-            model = "claude-3.7-sonnet",
-        },
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = {
+      { "zbirenbaum/copilot.lua" }, -- or zbirenbaum/copilot.lua
+      { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
     },
+    build = "make tiktoken", -- Only on MacOS or Linux
+    opts = {
+      model = "claude-3.7-sonnet",
+    },
+  },
 
-    -- Git
-    {
-        "tpope/vim-fugitive",
-        lazy = true,
-        event = "InsertEnter",
-    },
+  -- Git
+  {
+    "tpope/vim-fugitive",
+    lazy = true,
+    event = "InsertEnter",
+  },
 
-    -- Simple plugins can be specified as strings
-    { "rstacruz/vim-closer", lazy = true, event = "InsertEnter" },
+  -- Simple plugins can be specified as strings
+  { "rstacruz/vim-closer", lazy = true, event = "InsertEnter" },
 
-    -- lsp
-    {
-        "mason-org/mason-lspconfig.nvim",
-        opts = {},
-        dependencies = {
-            { "mason-org/mason.nvim", opts = {} },
-            "neovim/nvim-lspconfig",
-        },
+  -- lsp
+  {
+    "mason-org/mason-lspconfig.nvim",
+    dependencies = {
+      { "mason-org/mason.nvim", opts = {} },
+      "neovim/nvim-lspconfig",
     },
+  },
 
-    {
-        "jay-babu/mason-null-ls.nvim",
-        lazy = true,
-        -- event = { "BufReadPre", "BufNewFile" },
-    },
-    { "nvimtools/none-ls.nvim", lazy = true },
+  {
+    "jay-babu/mason-null-ls.nvim",
+    lazy = true,
+    -- event = { "BufReadPre", "BufNewFile" },
+  },
+  { "nvimtools/none-ls.nvim", lazy = true },
 
-    -- cmp plugins
-    { "hrsh7th/cmp-cmdline", event = "CmdlineEnter" },
-    { "hrsh7th/cmp-nvim-lua", lazy = true },
-    { "hrsh7th/cmp-nvim-lsp", lazy = true },
-    { "hrsh7th/cmp-buffer", lazy = true },
-    { "hrsh7th/cmp-path", lazy = true },
-    { "hrsh7th/nvim-cmp", lazy = true, event = "InsertEnter" },
-    {
-        "L3MON4D3/LuaSnip",
-        -- follow latest release.
-        version = "v2.3", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-        -- install jsregexp (optional!).
-        build = "make install_jsregexp",
-        lazy = true,
-        event = "InsertEnter",
-    },
-    { "saadparwaiz1/cmp_luasnip", lazy = true, event = "InsertEnter" },
+  -- cmp plugins
+  { "hrsh7th/cmp-cmdline", event = "CmdlineEnter" },
+  { "hrsh7th/cmp-nvim-lua", lazy = true },
+  { "hrsh7th/cmp-nvim-lsp", lazy = true },
+  { "hrsh7th/cmp-buffer", lazy = true },
+  { "hrsh7th/cmp-path", lazy = true },
+  { "hrsh7th/nvim-cmp", lazy = true, event = "InsertEnter" },
+  {
+    "L3MON4D3/LuaSnip",
+    -- follow latest release.
+    version = "v2.3", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+    -- install jsregexp (optional!).
+    build = "make install_jsregexp",
+    lazy = true,
+    event = "InsertEnter",
+  },
+  { "saadparwaiz1/cmp_luasnip", lazy = true, event = "InsertEnter" },
 
-    -- nvim-treesitter
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate",
-    },
+  -- nvim-treesitter
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+  },
 
-    -- dap
-    {
-        "mfussenegger/nvim-dap",
-        lazy = true,
-    },
-    {
-        "rcarriga/nvim-dap-ui",
-        dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
-        lazy = true,
-    },
-    {
-        "theHamsta/nvim-dap-virtual-text",
-        lazy = true,
-    },
-    {
-        "jay-babu/mason-nvim-dap.nvim",
-        lazy = true,
-    },
+  -- dap
+  {
+    "mfussenegger/nvim-dap",
+    lazy = true,
+  },
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
+    lazy = true,
+  },
+  {
+    "theHamsta/nvim-dap-virtual-text",
+    lazy = true,
+  },
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    lazy = true,
+  },
 
-    -- lspsaga
-    {
-        "nvimdev/lspsaga.nvim", -- LSP UIs
-        dependencies = {
-            "nvim-tree/nvim-web-devicons",
-        },
+  -- lspsaga
+  {
+    "nvimdev/lspsaga.nvim", -- LSP UIs
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
     },
+  },
 
-    {
-        "stevearc/conform.nvim",
-        event = { "BufWritePre" }, -- ファイル保存前にイベントを発火
-        cmd = { "ConformInfo" },
-        opts = {},
-    },
+  -- formatter
+  {
+    "stevearc/conform.nvim",
+    dependencies = { "williamboman/mason.nvim" },
+    lazy = true,
+    event = { "BufWritePre" }, -- ファイル保存前にイベントを発火
+    cmd = { "ConformInfo" },
+    opts = {},
+  },
 
-    -- file finder
-    {
-        "nvim-telescope/telescope.nvim",
-        lazy = true,
-    },
-    {
-        "nvim-telescope/telescope-file-browser.nvim",
-        lazy = true,
-    },
+  -- file finder
+  {
+    "nvim-telescope/telescope.nvim",
+    lazy = true,
+  },
+  {
+    "nvim-telescope/telescope-file-browser.nvim",
+    lazy = true,
+  },
 
-    -- comments
-    {
-        "numToStr/Comment.nvim",
-        opts = {},
-        event = "VimEnter",
-    },
+  -- comments
+  {
+    "numToStr/Comment.nvim",
+    opts = {},
+    event = "VimEnter",
+  },
 
-    --rust
-    {
-        "rust-lang/rust.vim",
-        lazy = true,
-        ft = { "rust" },
-    },
-    {
-        "mrcjkb/rustaceanvim",
-        version = "^5",
-        lazy = true,
-        ft = { "rust" },
-    },
+  --rust
+  {
+    "rust-lang/rust.vim",
+    lazy = true,
+    ft = { "rust" },
+  },
+  {
+    "mrcjkb/rustaceanvim",
+    version = "^5",
+    lazy = true,
+    ft = { "rust" },
+  },
 
-    -- statusline style
-    {
-        "nvim-lualine/lualine.nvim",
-        dependencies = {
-            "nvim-tree/nvim-web-devicons",
-        },
-        opts = {
-            theme = "material",
-            icons_enabled = true,
-        },
-        lazy = true,
-        event = "VimEnter",
+  -- statusline style
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
     },
-
-    -- cpp
-    {
-        "p00f/clangd_extensions.nvim",
-        lazy = true,
-        ft = { "c", "cpp" },
+    opts = {
+      theme = "material",
+      icons_enabled = true,
     },
+    lazy = true,
+    event = "VimEnter",
+  },
 
-    {
-        "lukas-reineke/indent-blankline.nvim",
-        main = "ibl",
-        ---@module "ibl"
-        ---@type ibl.config
-        lazy = true,
-        event = "VimEnter",
-        opts = {},
+  -- cpp
+  {
+    "p00f/clangd_extensions.nvim",
+    lazy = true,
+    ft = { "c", "cpp" },
+  },
+
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    ---@module "ibl"
+    ---@type ibl.config
+    lazy = true,
+    event = "VimEnter",
+    opts = {},
+  },
+
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+
+  -- git diff
+  { "sindrets/diffview.nvim", lazy = true, event = "VimEnter" },
+
+  {
+    "kdheepak/lazygit.nvim",
+    lazy = true,
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
     },
-
-    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-
-    -- git diff
-    { "sindrets/diffview.nvim", lazy = true, event = "VimEnter" },
-
-    {
-        "kdheepak/lazygit.nvim",
-        lazy = true,
-        cmd = {
-            "LazyGit",
-            "LazyGitConfig",
-            "LazyGitCurrentFile",
-            "LazyGitFilter",
-            "LazyGitFilterCurrentFile",
-        },
-        -- optional for floating window border decoration
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-        },
-        -- setting the keybinding for LazyGit with 'keys' is recommended in
-        -- order to load the plugin when the command is run for the first time
-        keys = {
-            { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
-        },
+    -- optional for floating window border decoration
+    dependencies = {
+      "nvim-lua/plenary.nvim",
     },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+    },
+  },
 })
--- plugin/conform.lua
+++ plugin/conform.lua
@@ -1,46 +1,50 @@
 require("conform").setup({
-    format_on_save = {
-        -- These options will be passed to conform.format()
-        timeout_ms = 500,
-        lsp_format = "fallback",
-        lsp_fallback = true,
-    },
-    formatters = {
-        stylua = {
-            exe = "stylua",
-            args = {
-                "--indent-type=spaces",
-                "--indent-width=4",
-                "--search-parent-directories",
-                "--stdin-filepath",
-            },
-            stdin = true,
-        },
-        black = {
-            exe = "black",
-            args = {
-                "-l 119",
-                "--quiet",
-            },
-            stdin = true,
-        },
-    },
-    formatters_by_ft = {
-        lua = { "stylua" },
+  formatters_by_ft = {
+    -- lua
+    lua = { "stylua" },
 
-        -- Conform will run multiple formatters sequentially
-        python = { "black" },
+    -- python
+    python = { "black" },
 
-        -- You can customize some of the format options for the filetype (:help conform.format)
-        rust = { "rustfmt", lsp_format = "fallback" },
+    -- rust
+    rust = { "rustfmt", lsp_format = "fallback" },
 
-        -- Conform will run the first available formatter
-        typescript = { "biome" },
-        typescriptreact = { "biome" },
-        javascript = { "biome" },
-        javascriptreact = { "biome" },
-        json = { "biome" },
-        jsonc = { "biome" },
-        cs = { "csharpier" },
+    -- typescript
+    typescript = { "biome" },
+    typescriptreact = { "biome" },
+    javascript = { "biome" },
+    javascriptreact = { "biome" },
+    json = { "biome" },
+    jsonc = { "biome" },
+    cs = { "csharpier" },
+
+    -- go
+    go = { "goimports", "gofumpt" },
+  },
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_format = "fallback",
+    lsp_fallback = true,
+  },
+  formatters = {
+    stylua = {
+      exe = "stylua",
+      prepend_args = {
+        "--indent-type=spaces",
+        "--indent-width=2",
+        "-",
+      },
+      stdin = true,
     },
+    black = {
+      exe = "black",
+      prepend_args = {
+        "-l 120",
+        "--quiet",
+        "-",
+      },
+      stdin = true,
+    },
+  },
 })
