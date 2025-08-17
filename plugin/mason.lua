4754029507547acb156272bc99fd3b9e5d9d8fc7
OTakumi <takumi.o.0410@gmail.com>
2025-08-17 17:56:47 +0900

    feat: masonとnull-lsの設定をインデント修正
    mason-nvim-dapおよびmason-null-ls、null-lsの設定ファイルで
    インデントをスペース2つに統一し、可読性を向上させました。
    機能自体の変更はありません。

--- plugin/mason.lua
+++ plugin/mason.lua
@@ -1,53 +1,51 @@
 require("mason-nvim-dap").setup({
-    ensure_installed = {
-        "python",
-        "delve",
-        "cppdbg",
-        "codelldb",
-        "cpptools",
-    },
-    automatic_installation = true,
+  ensure_installed = {
+    "python",
+    "delve",
+    "cppdbg",
+    "codelldb",
+    "cpptools",
+  },
+  automatic_installation = true,
 })
 
 require("mason-null-ls").setup({
-    function() end,
-    ensure_installed = {
-        "biome",
-        "cpplint",
-        "clang-format",
-        "gofmt",
-        "gofumpt",
-        "goimports",
-        "hadolint",
-        "markdownlint",
-        "prettier",
-        "stylua",
-        "stylelint",
-        "terraform",
-        "yamllint",
-        "yamlfmt",
-    },
-    automatic_installation = true,
+  function() end,
+  ensure_installed = {
+    "biome",
+    "cpplint",
+    "clang-format",
+    "gofmt",
+    "gofumpt",
+    "goimports",
+    "hadolint",
+    "markdownlint",
+    "stylua",
+    "stylelint",
+    "terraform",
+    "yamllint",
+    "yamlfmt",
+  },
+  automatic_installation = true,
 })
 
 local null_ls = require("null-ls")
 null_ls.setup({
-    sources = {
-        -- linters
-        null_ls.builtins.diagnostics.hadolint,
-        null_ls.builtins.diagnostics.yamllint,
-        null_ls.builtins.diagnostics.hadolint,
+  sources = {
+    -- linters
+    null_ls.builtins.diagnostics.hadolint,
+    null_ls.builtins.diagnostics.yamllint,
+    null_ls.builtins.diagnostics.hadolint,
 
-        -- formatters
-        null_ls.builtins.formatting.isort,
-        null_ls.builtins.formatting.biome,
-        null_ls.builtins.formatting.clang_format,
-        null_ls.builtins.formatting.gofmt,
-        null_ls.builtins.formatting.gofumpt,
-        null_ls.builtins.formatting.goimports,
-        null_ls.builtins.formatting.markdownlint,
-        null_ls.builtins.formatting.prettierd,
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.yamlfmt,
-    },
+    -- formatters
+    null_ls.builtins.formatting.isort,
+    null_ls.builtins.formatting.biome,
+    null_ls.builtins.formatting.clang_format,
+    null_ls.builtins.formatting.gofmt,
+    null_ls.builtins.formatting.gofumpt,
+    null_ls.builtins.formatting.goimports,
+    null_ls.builtins.formatting.markdownlint,
+    null_ls.builtins.formatting.stylua,
+    null_ls.builtins.formatting.yamlfmt,
+  },
 })
