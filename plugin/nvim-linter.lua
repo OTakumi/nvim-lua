97c95c506733aaed2131f797d8f0d9fcad9496fe
OTakumi <takumi.o.0410@gmail.com>
2025-08-28 18:17:44 +0900

    feat: nvim-linterの初期設定を追加
    各言語用のリンターを設定し、言語ごとに適切なリンターを割り当てました。
    JavaScript/TypeScript、Go、Lua、Python、Docker、Markdown、SQL、
    YAMLファイルに対応するリンターを設定しています。

---
+++ plugin/nvim-linter.lua
@@ -0,0 +1,22 @@
+local lint = require("lint")
+lint.linters_by_ft = {
+  javascript = { "biomejs" },
+  typescript = { "biomejs" },
+  jsx = { "biomejs" },
+  tsx = { "biomejs" },
+  go = { "golangcilint" },
+  lua = { "luacheck" },
+  python = {
+    ruff = {
+      args = {
+        "--select=F,E",
+        "--ignore=E501",
+        "--line-length=120",
+      },
+    },
+  },
+  docker = { "hadolint" },
+  markdown = { "vale" },
+  sql = { "sqlfluff" },
+  yaml = { "yq" },
+}
