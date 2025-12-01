08f396d41bac94edb34be7cb071032bb89f4f215
OTakumi <takumi.o.0410@gmail.com>
2025-12-01 16:04:01 +0900

    feat: nvim-surroundプラグインを追加
    nvim-surroundプラグイン（v3.0.0）をVeryLazyイベントで追加しました。

--- lua/lazy/lazy.lua
+++ lua/lazy/lazy.lua
@@ -185,6 +185,14 @@ require("lazy").setup({
   },
 
   {
+    "kylechui/nvim-surround",
+    version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    opts = {
+      -- Configuration here, or leave empty to use defaults
+    },
+  },
+  {
     "windwp/nvim-autopairs",
     event = "InsertEnter",
     config = true,
