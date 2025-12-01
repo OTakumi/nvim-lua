4bb06c884a934767792d20192e3f458739355b49
OTakumi <takumi.o.0410@gmail.com>
2025-12-01 18:35:59 +0900

    feat: ファイラーoil.nvimを追加
    新しいファイラープラグインoil.nvimを導入しました。

--- lua/lazy/lazy.lua
+++ lua/lazy/lazy.lua
@@ -176,6 +176,17 @@ require("lazy").setup({
     "nvim-telescope/telescope-file-browser.nvim",
     lazy = true,
   },
+  {
+    "stevearc/oil.nvim",
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {},
+    -- Optional dependencies
+    dependencies = { { "nvim-mini/mini.icons", opts = {} } },
+    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+    lazy = false,
+  },
 
   -- comments
   {
