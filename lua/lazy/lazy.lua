466ebd1dbcd9534ed4330c4e64fef7a5883b8d81
OTakumi <takumi.o.0410@gmail.com>
2025-12-01 15:44:21 +0900

    feat: nvim-treeプラグインの削除
    nvim-tree/nvim-tree.luaプラグインと関連依存を設定から削除しました。

--- lua/lazy/lazy.lua
+++ lua/lazy/lazy.lua
@@ -173,20 +173,6 @@ require("lazy").setup({
     lazy = true,
   },
 
-  -- tree
-  {
-    "nvim-tree/nvim-tree.lua",
-    version = "*",
-    lazy = false,
-    dependencies = {
-      "nvim-tree/nvim-web-devicons",
-    },
-    config = function()
-      require("nvim-tree").setup({})
-    end,
-    opts = {},
-  },
-
   -- comments
   {
     "numToStr/Comment.nvim",
