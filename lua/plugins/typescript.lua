ebc91e0deacc04efb0b7f5bc9f733faa966d4701
OTakumi <takumi.o.0410@gmail.com>
2025-12-23 23:12:47 +0900

    refactor(typescript): typescript 設定ファイルを削除

--- lua/plugins/typescript.lua
+++
@@ -1,8 +0,0 @@
-return {
-  -- typescript-tools.nvim をコメントアウト（vtsls を使用するため）
-  -- {
-  --   "pmizio/typescript-tools.nvim",
-  --   dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-  --   opts = {},
-  -- },
-}
