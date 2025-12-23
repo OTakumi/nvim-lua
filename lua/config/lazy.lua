468cc1377e33e6767d38a128d11801951c1dc102
OTakumi <takumi.o.0410@gmail.com>
2025-12-23 20:20:43 +0900

    feat(config): リーダーキーをスペースからカンマに変更し設定例をコメント追加
    - リーダーキーをスペースからカンマに変更し、操作性を向上
    - 現在のリーダーキー確認方法をコメントで追記

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -18,7 +18,9 @@ vim.opt.rtp:prepend(lazypath)
 -- Make sure to setup `mapleader` and `maplocalleader` before
 -- loading lazy.nvim so that mappings are correct.
 -- This is also a good place to setup other settings (vim.opt)
-vim.g.mapleader = " "
+-- You can check the currently configured leader with the following command:
+-- :lua print(vim.inspect(vim.g.mapleader))
+vim.g.mapleader = ","
 vim.g.maplocalleader = "\\"
 
 -- Setup lazy.nvim
