8ea49e3d447dc3e1f9289669bd03c83394b8c46d
OTakumi <takumi.o.0410@gmail.com>
2025-12-25 21:56:45 +0900

    feat(core): relativenumberオプションを追加し、インデントを統一しました
    - relativenumberを有効化し、行番号の表示方法を改善
    - インデントをスペース2つに統一し、可読性を向上

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,35 +1,36 @@
 local options = {
-    encoding = "utf-8",
-    fileencoding = "utf-8",
-    title = true,
-    backup = false,
-    autoindent = true,
-    tabstop = 4,
-    softtabstop = 4,
-    shiftwidth = 4,
-    colorcolumn = "80",
-    termguicolors = true,
-    number = true,
-    list = true,
-    ignorecase = true,
-    smartcase = true,
-    wrapscan = true,
-    hlsearch = true,
-    incsearch = true,
-    inccommand = "split",
-    cursorcolumn = true,
-    signcolumn = "yes",
-    expandtab = true,
+  encoding = "utf-8",
+  fileencoding = "utf-8",
+  title = true,
+  backup = false,
+  autoindent = true,
+  tabstop = 4,
+  softtabstop = 4,
+  shiftwidth = 4,
+  colorcolumn = "80",
+  termguicolors = true,
+  relativenumber = true,
+  number = true,
+  list = true,
+  ignorecase = true,
+  smartcase = true,
+  wrapscan = true,
+  hlsearch = true,
+  incsearch = true,
+  inccommand = "split",
+  cursorcolumn = true,
+  signcolumn = "yes",
+  expandtab = true,
 }
 
 vim.opt.shortmess:append("c")
 for k, v in pairs(options) do
-    vim.opt[k] = v
+  vim.opt[k] = v
 end
 
 vim.opt.listchars = {
-    space = "_",
-    tab = ">-",
+  space = "_",
+  tab = ">-",
 }
 
 vim.opt.clipboard:append({ "unnamedplus" })
