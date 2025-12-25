320ff8ff5c7c51f421b3fb6ef088f5218e5cdf20
OTakumi <takumi.o.0410@gmail.com>
2025-12-25 22:47:05 +0900

    feat(telescope): file_browser拡張のキーマップ追加とコメント整理
    - <space>fbでfile_browserをdropdownテーマで起動するキーマップを追加
    - コメントを日本語から英語に統一し、可読性を向上
    - telescope拡張機能の読み込み部分を整理

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -18,13 +18,12 @@ return {
       local telescope = require("telescope")
       telescope.setup(opts)
 
-      -- 拡張機能の読み込み
       telescope.load_extension("fzf")
       telescope.load_extension("ghq")
       telescope.load_extension("z")
       telescope.load_extension("file_browser")
 
-      -- 一般的なキーマップを追加
+      -- keymaps
       local builtin = require("telescope.builtin")
       vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope: Find Files" })
       vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope: Live Grep" })
@@ -32,6 +31,12 @@ return {
       vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope: Help Tags" })
       vim.keymap.set("n", "<leader>fz", "<cmd>Telescope z<cr>", { desc = "Telescope: Z" })
       vim.keymap.set("n", "<leader>fq", "<cmd>Telescope ghq<cr>", { desc = "Telescope: GHQ" })
+      vim.keymap.set(
+        "n",
+        "<space>fb",
+        ":Telescope file_browser path=%:p:h select_buffer=true theme=dropdown<CR>",
+        { desc = "Telescope: File Browser" }
+      )
     end,
   },
 }
